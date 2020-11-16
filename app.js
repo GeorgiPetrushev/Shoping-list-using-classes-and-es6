798a5b68f06536278f6b6a2bfa3224bfeecb1afa
George <figoto660@gmail.com>
2020-11-16 12:28:00 -0800

    add style

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ class Button{
     constructor(_name,_className,_type){
         this.btn = document.createElement(_type);
         this.btn.className = `buttons ${_className}`;
-        this.btn.textContent = _name;
-        this.selectHeader = document.querySelector('#shopList');
-        this.selectHeader.appendChild(this.btn);    
+        this.btn.textContent = _name;  
     }
-
+    
+    show(where){
+        let selectPlaice = document.querySelector(`${where}`);
+        selectPlaice.appendChild(this.btn);
+    }
 }
 
 class List{
@@ -25,7 +27,7 @@ class List{
         this.clearTheList();
         for(const text of this.list){
             let li = document.createElement("li");
-            li.textContent = text;    
+            li.innerHTML = '<h1>'+text+'</h1>';    
             this.item.appendChild(li);
         }
     }
@@ -33,13 +35,31 @@ class List{
     add(_newItem){
         let li = document.createElement('li');
         li.textContent = _newItem;
+        li.className = `${_newItem}`;
         this.item.appendChild(li);
         this.list.push(_newItem);
+        let deleteButton = new Button('Delete Item','remove','BUTTON'); 
+   
+        li.addEventListener("click",() => {
+            li.style.backgroundColor = 'orange';
+            li.style.borderRadius = '15px';
+            li.appendChild(deleteButton.btn);
+        });
+        li.addEventListener('dblclick',(e) => {
+            li.style.backgroundColor = null;
+            deleteButton.btn.remove();            
+        });
+        deleteButton.btn.addEventListener('click',() => {
+            li.appendChild(deleteButton.btn);
+            li.remove();
+        });
     }
+
     removeItem(_itemRemove){
         this.list = this.list.filter(e => e.toUpperCase() !== _itemRemove.toUpperCase());
         this.updateScreen();
     }
+
     restartItems(){
         this.clearTheList();
         this.list.length = 0;                
@@ -48,9 +68,11 @@ class List{
 
 let x = new List();
 let addButton = new Button('Add Item','add','BUTTON');
-let removeButton = new Button('Remove Item','remove','BUTTON');
+addButton.show('#shopList');
 let clearButton = new Button('Clear List','clear','BUTTON');
+clearButton.show('#shopList');
 let textPlaice = new Button('test','textPlaice','INPUT');
+textPlaice.show('#shopList');
 
 x.add('Milk');
 x.add('Banana');
@@ -62,10 +84,10 @@ addButton.btn.addEventListener("click", function(){
     let value = document.getElementsByTagName('INPUT')[0].value; 
     x.add(value);
 });
-removeButton.btn.addEventListener("click", function(){
-    let value = document.getElementsByTagName('INPUT')[0].value;
-    x.removeItem(value);
-});
+// removeButton.btn.addEventListener("click", function(){
+//     let value = document.getElementsByTagName('INPUT')[0].value;
+//     x.removeItem(value);
+// });
 clearButton.btn.addEventListener("click", function(){
     x.restartItems();
 });
